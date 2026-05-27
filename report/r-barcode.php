7b9179d0cc1c3ef2579ff9729fb3f71e4a28d010
Rendymatheu <rendymatheu1@gmail.com>
2026-05-27 16:14:38 +0700

    update barcode2

--- report/r-barcode.php
+++ report/r-barcode.php
@@ -1,4 +1,6 @@
 <?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 
 session_start();
 
@@ -6,45 +8,46 @@ if (!isset($_SESSION["ssLoginPOS"])) {
     header("location: ../auth/login.php");
     exit();
 }
-
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Print Barcode</title>
 </head>
-
 <body>
     <?php
-
     $jmlCetak = $_GET["jmlCetak"];
+    $barcode  = $_GET['barcode'];
+
+    $path1 = __DIR__ . '/../../vendor/autoload.php';
+    $path2 = __DIR__ . '/../asset/barcodeGenerator/vendor/autoload.php';
+    $path3 = __DIR__ . '/../vendor/autoload.php';
+
+    if (file_exists($path1)) {
+        require $path1;
+        echo "<!-- path1 ok -->";
+    } elseif (file_exists($path2)) {
+        require $path2;
+        echo "<!-- path2 ok -->";
+    } elseif (file_exists($path3)) {
+        require $path3;
+        echo "<!-- path3 ok -->";
+    } else {
+        die("❌ vendor/autoload.php tidak ditemukan!");
+    }
+
     for ($i = 1; $i <= $jmlCetak; $i++) { ?>
-        <div style="text-align: center; width: 210px; float: left; 
-        margin-right:30; margin-bottom:30px;">
+        <div style="text-align:center; width:210px; float:left; margin-right:30px; margin-bottom:30px;">
             <?php
-
-            $barcode = $_GET['barcode'];
-
-            require __DIR__ . '/../../vendor/autoload.php';
-
             $generator = new Picqer\Barcode\BarcodeGeneratorPNG();
-            echo '<img src="data:image/png;base64,' . base64_encode($generator->getBarcode($barcode, $generator::TYPE_CODE_128)) . '"
-            width="200px">';
+            echo '<img src="data:image/png;base64,' . base64_encode($generator->getBarcode($barcode, $generator::TYPE_CODE_128)) . '" width="200px">';
             ?>
             <div><?= $barcode ?></div>
         </div>
-    <?php
-    }
-    ?>
+    <?php } ?>
 
-    <script>
-        window.print();
-    </script>
+    <script>window.print();</script>
 </body>
-
 </html>
